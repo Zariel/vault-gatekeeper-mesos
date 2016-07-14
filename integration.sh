b899c404664eb9c89b7f575a13404a250d9d4d0c
Nimi Wariboko Jr <nimi@channelmeter.com>
2016-07-14 13:56:18 -0700

    travis

---
+++ integration.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+function vault () {
+	docker run -it --rm -e VAULT_ADDR --entrypoint=/bin/sh sjourdan/vault -c "vault auth $VAULT_TOKEN &>/dev/null; vault $*"
+}
+
+function run_tests() {
+
+	# Run Vault
+	docker run -d -p 8200:8200 --hostname vault --name vault sjourdan/vault
+	VAULT_HOST=`docker inspect -f '{{ .NetworkSettings.IPAddress }}' vault`
+	export VAULT_ADDR="http://$VAULT_HOST:8200"
+	export VAULT_TOKEN=`docker logs vault 2>/dev/null | grep 'Root Token' | awk '{ printf $3 }'`
+
+	# Import minimesos env args
+	eval `cd minimesos/ && minimesos info | tail -n+3`
+
+	vault status
+}
+
+run_tests
